63c0fa81d4b9ddbdecdb8ee753717811cc44b9b6
inokawa <48897392+inokawa@users.noreply.github.com>
2023-05-30 20:44:54 +0900

    3.6

--- src/index.ts
+++ src/index.ts
@@ -55,14 +55,16 @@ const cellShaderModule = device.createShaderModule({
           @vertex
           fn vertexMain(@location(0) pos: vec2f,
                         @builtin(instance_index) instance: u32) ->
-            @builtin(position) vec4f {
-
-            let i = f32(instance); // Save the instance_index as a float
-            let cell = vec2f(i, i);
-            let cellOffset = cell / grid * 2; // Updated
-            let gridPos = (pos + 1) / grid - 1 + cellOffset;
-
-            return vec4f(gridPos, 0, 1);
+          @builtin(position) vec4f {
+        
+          let i = f32(instance);
+          // Compute the cell coordinate from the instance_index
+          let cell = vec2f(i % grid.x, floor(i / grid.x));
+        
+          let cellOffset = cell / grid * 2;
+          let gridPos = (pos + 1) / grid - 1 + cellOffset;
+        
+          return vec4f(gridPos, 0, 1);
           }
 
           @fragment
