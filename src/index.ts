576b9bcbaf3cd313d624419e6908f5cccb7bea60
inokawa <48897392+inokawa@users.noreply.github.com>
2023-06-18 12:58:19 +0900

    Separate .wgsl

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import shader from "./shader.wgsl?raw";
+
 const canvas = document.querySelector("canvas")!;
 
 // WebGPU device initialization
@@ -49,37 +51,7 @@ const vertexBufferLayout: GPUVertexBufferLayout = {
 // Create the shader that will render the cells.
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
-  code: `
-          struct VertexInput {
-            @location(0) pos: vec2f,
-            @builtin(instance_index) instance: u32,
-          };
-          
-          struct VertexOutput {
-            @builtin(position) pos: vec4f,
-            @location(0) cell: vec2f, // New line!
-          };
-          
-          @group(0) @binding(0) var<uniform> grid: vec2f;
-          
-          @vertex
-          fn vertexMain(input: VertexInput) -> VertexOutput  {
-            let i = f32(input.instance);
-            let cell = vec2f(i % grid.x, floor(i / grid.x));
-            let cellOffset = cell / grid * 2;
-            let gridPos = (input.pos + 1) / grid - 1 + cellOffset;
-          
-            var output: VertexOutput;
-            output.pos = vec4f(gridPos, 0, 1);
-            output.cell = cell; // New line!
-            return output;
-          }
-
-          @fragment
-          fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
-            return vec4f(input.cell/grid, 0, 1);
-          }
-        `,
+  code: shader,
 });
 
 // Create a pipeline that renders the cell.
